47e8cd072a4e78ca710088a846e655cbcc91c0e5
xfzhang0602 <1505528051@qq.com>
2024-07-14 00:05:38 +0800

    20240714

---
+++ 20240713_demo1/demo1.py
@@ -0,0 +1,10 @@
+# @Time: 2024/7/13 
+# @Author:xfzhang0602
+# @File:demo1.py
+# @Software: PyCharm
+
+
+# '''
+# print("Hello World")
+# '''
+# print('Hello World')
---
+++ 20240713_demo1/demo2.py
@@ -0,0 +1,83 @@
+# @Time: 2024/7/13 
+# @Author:xfzhang0602
+# @File:demo2.py
+# @Software: PyCharm
+
+#1.变量的定义
+a=10     #整数
+b="张三"  #字符串
+c=2000.00 #浮点数，小数
+
+#见名知义
+age=10
+name="张三"
+money=2000.00
+
+
+
+'''
+_age=10
+ZhangsanAge=10
+zhangsan_age=10
+money2000=2000.00
+
+_="hehe"
+print(_)
+
+年龄=3
+名字="鸽鸽"
+print('名字',名字)
+
+
+#变量的赋值与类型
+
+age=10
+name="张三"
+money=2000.00
+
+print(type(age))   #<class 'int'>
+print(type(name))  #<class 'str'>
+print(type(money)) #<class 'float'>
+
+a=b=c=100
+print(a)
+print(b)
+print(c)
+
+
+a,b,c=1,100,"QAQ"
+print(a)
+print(b)
+print(c)
+
+import keyword
+print(keyword.kwlist)   #['False', 'None', 'True', 'and', 'as',
+                        # 'assert', 'async', 'await', 'break', 'class',
+                        # 'continue', 'def', 'del', 'elif', 'else',
+                        # 'except', 'finally', 'for', 'from', 'global',
+                        # 'if', 'import', 'in', 'is', 'lambda', 'nonlocal',
+                        # 'not', 'or', 'pass', 'raise', 'return', 'try', 'while',
+                     # 'with', 'yield']
+as=100
+print(as)
+  File "D:\code\Python\20240713_demo1\demo2.py", line 58
+    as=100
+    ^^
+SyntaxError: invalid syntax
+
+as1=100
+print(as1)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
